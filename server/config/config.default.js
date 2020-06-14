ecedd6705672293d639df2adbb8fea7eeef695f6
stormer.xia <stormer.xia@pkfare.com>
2020-06-14 15:31:52 +0800

    UPDATE:关闭CSRF

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -25,6 +25,11 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
